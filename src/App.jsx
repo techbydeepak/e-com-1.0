cc658eec3783a7dd40c5f4005083617cb63b67d9
techbydeepak <deepakrathour05tech@gmail.com>
2026-02-06 14:37:54 +0530

    fix vite base for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Register from './pages/home/register/Register'
 import Login from './pages/home/login/Login'
 import AllProducts from './pages/home/all products/AllProducts'
 import SingleProduct from './pages/home/single product/SingleProduct'
 import Cart from './pages/home/cart/Cart'
 
-function App() {
-  
+// Ye dynamic REPO_BASE use karo
+const REPO_BASE = import.meta.env.PROD ? '/namandigital_task3_e_com' : ''
 
+function App() {
   return (
-    <Router>
+    <Router basename={REPO_BASE}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/products" element={<AllProducts/>}/>
-        <Route path="/product/:id" element={<SingleProduct/>}/>
-        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<AllProducts />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   )
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: mode === 'production' ? '/namandigital_task3_e_com/' : '/',
-}))
+  base: '/',
+})
